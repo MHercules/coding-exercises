10a965b5736184263caa6e4799cbb873913d6495
MHercules <100857295+MHercules@users.noreply.github.com>
2023-12-12 21:47:18 +0200

    Add files via upload

---
+++ HelloPeopleOf.java
@@ -0,0 +1,16 @@
+//12/12/23
+
+public class HelloPeopleOf
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Failure is not the end, giving up is.") 
+	}
+	//Can't seem to focus tonight so going to rest early.
+	//Struggling more with mathematical problems, which is frustrating
+	//Can't get past a question, but coming back to it tomorrow
+	/*Write a program that accepts an integer number of minutes from a user and converts it both to hours 
+	and days. For example, 6,000 minutes equals 100 hours and equals 4.167 days. Save the program as 
+	MinutesConversion.java*/
+	//Peace.
+}
