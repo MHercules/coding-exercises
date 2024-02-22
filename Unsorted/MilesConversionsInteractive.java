c78905c8eed750c8afdb6ff075c599f6e869b6f2
Marc <mivhercules@gmail.com>
2024-02-22 13:40:14 +0200

    Update and rename MilesConversionsInteractive.java to Unsorted/MilesConversionsInteractive.java

--- Unsorted/MilesConversionsInteractive.java
+++ Unsorted/MilesConversionsInteractive.java
@@ -7,7 +7,7 @@ public class MilesConversionsInteractive
 {
 	public static void main(String[] args)
 	{
-		final int INCHES_IN_MILE = 63360;
+	final int INCHES_IN_MILE = 63360;
         final int FEET_IN_MILE = 5280;
         final int YARDS_IN_MILE = 1760;
         int miles;
